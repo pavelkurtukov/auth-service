81b32e8eb9ed81206fae055063340172bf606f10
pavelkurtukov <pavel@kurtukov.ru>
2024-03-11 00:08:51 +0700

    Обработчик исключений

---
+++ src/main/java/ru/kurtukov/authservice/exception/ExceptionHandlerAdvice.java
@@ -0,0 +1,21 @@
+package ru.kurtukov.authservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> invalidCredentialsHandler(InvalidCredentials e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> unauthorizedUserHandler(UnauthorizedUser e) {
+        System.out.println(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
