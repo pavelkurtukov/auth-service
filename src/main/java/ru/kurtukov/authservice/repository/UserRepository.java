7057065a07a6c8f1bb313f79c4ab3a71bf42b52e
pavelkurtukov <pavel@kurtukov.ru>
2024-03-10 23:56:29 +0700

    Реализация UserRepository

--- src/main/java/ru/kurtukov/authservice/repository/UserRepository.java
+++ src/main/java/ru/kurtukov/authservice/repository/UserRepository.java
@@ -2,12 +2,33 @@ package ru.kurtukov.authservice.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.kurtukov.authservice.model.Authorities;
+import ru.kurtukov.authservice.model.User;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
 public class UserRepository {
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return null;
+        System.out.println("getUserAuthorities");
+        List<Authorities> authoritiesList = new ArrayList<>();
+
+        // Список пользователей и их прав
+        HashMap<String, User> users = new HashMap<>();
+        users.put("admin", new User("admin", "admin", List.of(Authorities.READ, Authorities.WRITE, Authorities.DELETE)));
+        users.put("pavel", new User("pavel", "pavel", List.of(Authorities.READ, Authorities.WRITE, Authorities.DELETE)));
+        users.put("user", new User("user", "user", List.of(Authorities.READ, Authorities.WRITE)));
+        users.put("guest", new User("guest", "guest", List.of(Authorities.READ)));
+
+        // Проверка наличия пользователя, а также логина и пароля
+        if (users.containsKey(user)) {
+            User checkUser = users.get(user);
+            if (checkUser.getPassword().equals(password)) {
+                authoritiesList = checkUser.getAuthoritiesList();
+            }
+        }
+
+        return authoritiesList;
     }
 }
